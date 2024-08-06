839b7e0a08a5d7dc598365edea6ab37862643ae7
harshshinde123 <harshashinde2503@gmail.com>
2024-08-07 01:52:47 +0530

    Project Backend Configuration

---
+++ backend/src/main/java/com/example/locker_management_system/LockerManagementSystemApplication.java
@@ -0,0 +1,15 @@
+package com.example.locker_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerManagementSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LockerManagementSystemApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ backend/src/test/java/com/example/locker_management_system/LockerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locker_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
