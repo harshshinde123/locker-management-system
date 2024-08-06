ce81943eddd798f9f10be7ecf82ca043149f3aff
harshshinde123 <harshashinde2503@gmail.com>
2024-08-07 01:54:37 +0530

    added custom exception

---
+++ backend/src/main/java/com/example/locker_management_system/CustomException/UserException.java
@@ -0,0 +1,7 @@
+package com.example.locker_management_system.CustomException;
+
+public class UserException extends RuntimeException {
+    public UserException(String message) {
+        super(message);
+    }
+}
